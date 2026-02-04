3e1ece65fbecc9091281f6cb89326c204c5cbb92
shobhit-likeminds <shobhit.singh@likeminds.community>
2026-02-04 17:03:03 +0530

    change

---
+++ blocks/sample_component/sample_component.js
@@ -0,0 +1,7 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  console.log(block, "block");
+}
