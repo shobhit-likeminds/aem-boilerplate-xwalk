b346ca97a63e3b3236065711fb0beb859a99a34c
shobhit-likeminds <shobhit.singh@likeminds.community>
2026-02-23 11:51:28 +0530

    removed all non initial blocks

--- blocks/sample-component/sample-component.js
+++
@@ -1,7 +0,0 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
-export default function decorate(block) {
-  /* change to ul, li */
-  console.log(block, "block");
-}
