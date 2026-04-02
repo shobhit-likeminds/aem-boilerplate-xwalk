639995076ee6423d9a5739e9f21f3bbc0f3ee310
shobhit-likeminds <shobhit.singh@likeminds.community>
2026-04-02 09:51:13 +0530

    removed wrong components

--- blocks/space-adder/space-adder.js
+++
@@ -1,8 +0,0 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
-export default function decorate(block) {
-  // The Space-Adder block is empty and serves as a placeholder for spacing.
-  // It doesn't have any content to render, so we just ensure it has the correct classes.
-  block.classList.add('spaceAdder', 'aem-GridColumn', 'aem-GridColumn--default--12');
-}
